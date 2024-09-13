9d8e6b8fde14d03b75b14d1564a708ff9b66c0a8
Mircea Feder <federmircea@gmail.com>
2024-09-13 23:21:40 +0300

    sss

--- ui/src/components/DesignComponentsMenu/DesignComponentsMenu.tsx
+++ ui/src/components/DesignComponentsMenu/DesignComponentsMenu.tsx
@@ -4,7 +4,19 @@ import "./DesignComponentsMenu.scss"
 export const DesignComponentsMenu = () => {
     return (
         <div className="design-components-menu">
-            design components
+            <p className="menu-title">Components</p>
+            <div className="components-module generic-module">
+                <p className={"module-title"}>Generic</p>
+            </div>
+            <div className="components-module aws-module">
+                <p className={"module-title"}>AWS</p>
+            </div>
+            <div className="components-module gcp-module">
+                <p className={"module-title"}>GCP</p>
+            </div>
+            <div className="components-module azure-module">
+                <p className={"module-title"}>Azure</p>
+            </div>
         </div>
     );
 }
